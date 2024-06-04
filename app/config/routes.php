e857bf69221ed8d591a4651962c5745a16f178ad
Danilkberlin <danilkravets@gmail.com>
2024-06-04 11:26:26 +0200

    Filled a complete array with data in routes.php about further routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
+    //index
     '' => [
         'controller' => 'main',
         'action' => 'index'   
     ],
-
+    //Account 
     'account/login' => [
         'controller' => 'account',
         'action' => 'login'   
@@ -15,9 +16,38 @@ return [
         'controller' => 'account',
         'action' => 'register'   
     ],
-
+    //NEWS
     'new/show' => [
         'controller' => 'new',
         'action' => 'show'   
     ],
+
+    'new/list' => [
+        'controller' => 'new',
+        'action' => 'list'   
+    ],
+    //gallery
+    'gallery/show' => [
+        'controller' => 'gallery',
+        'action' => 'show'   
+    ],
+
+    'gallery/list' => [
+        'controller' => 'gallery',
+        'action' => 'list'   
+    ],
+    //Tournaments
+    'tournaments/show' => [
+        'controller' => 'tournaments',
+        'action' => 'show'   
+    ],
+
+    'tournaments/list' => [
+        'controller' => 'tournaments',
+        'action' => 'list'   
+    ],
+    'tournaments/live' => [
+        'controller' => 'tournaments',
+        'action' => 'live'   
+    ]
 ];
