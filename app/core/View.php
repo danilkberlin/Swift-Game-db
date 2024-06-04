adf71eed9af34e05aaf3728a38c6e079e0186912
Danilkberlin <danilkravets@gmail.com>
2024-06-04 13:30:03 +0200

    Wrote routing of all pages for View.php which performs the search property in the view folder

---
+++ app/core/View.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\core;
+
+class View{
+
+
+    public $path;
+    public $route;
+    public $layout  = 'default';
+    
+
+    public function __construct($route){
+        $this->route = $route;
+        $this->path = $route['controller'].'/'.$route['action'];
+    }
+
+    public function render($title, $vars = []){
+        ob_start();
+        require 'app/view/'.$this->path.'.php';
+        $content = ob_get_clean();
+
+        require 'app/view/layouts/'.$this->layout.'.php';
+    }
+}
