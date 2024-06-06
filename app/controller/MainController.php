94ba48abd289d35d83e11fc4dc0f6bd10304a089
Danilkberlin <danilkravets@gmail.com>
2024-06-06 11:59:48 +0200

    create new controller

--- app/controller/MainController.php
+++ app/controller/MainController.php
@@ -9,9 +9,11 @@ class MainController extends Controller {
     public function indexAction(){
 
         $db = new Db;
-        $data = $db->row('SELECT email FROM user');
+        $data = $db->column('SELECT email FROM user');
         debug($data);
         $this->view->render('Home');
 
     }
+
 }
+
