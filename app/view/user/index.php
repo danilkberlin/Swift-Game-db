67aeda167f0a544466be8301baf276918539dbd1
Danilkberlin <danilkravets@gmail.com>
2024-06-06 12:00:03 +0200

    default HTML

---
+++ app/view/user/index.php
@@ -0,0 +1,13 @@
+<ol>
+    <li>Zeige alle user an</li>
+    <li>Kreiere einen User</li>
+    <li>Lösche einen User</li>
+</ol>
+
+<form action=".." method="post">
+    <div class="mb-3">
+        <label for='email'>Your Emal</label>
+        <input name='email' class="form-control w-50 mb-3" type='email' id="email">
+        <button type="submit" class='btn btn-primary py-2 px-5'>Submit</button>
+    </div>
+</form>
