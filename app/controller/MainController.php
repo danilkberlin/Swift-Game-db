82bf8b964ccdee1e0a3aaae0d93cf47c616991c4
Danilkberlin <danilkravets@gmail.com>
2024-06-06 10:14:08 +0200

    An object of class Db is created, then an SQL query is executed using it to get the values ​​of the email field from the user table, and the result is printed using the debug function

--- app/controller/MainController.php
+++ app/controller/MainController.php
@@ -2,10 +2,16 @@
 
 namespace app\controller;
 use app\core\Controller;
+use app\lib\Db;
 
 class MainController extends Controller {
 
     public function indexAction(){
+
+        $db = new Db;
+        $data = $db->row('SELECT email FROM user');
+        debug($data);
         $this->view->render('Home');
+
     }
 }
