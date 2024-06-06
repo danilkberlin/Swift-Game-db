532c705a2fc2c5106b036df73422586a748bf8aa
Danilkberlin <danilkravets@gmail.com>
2024-06-06 10:12:31 +0200

    I created a class to retrieve information from my database tables.

--- app/lib/Db.php
+++ app/lib/Db.php
@@ -1,12 +1,30 @@
 <?php
 
 namespace app\lib;
+use PDO;
 
-class Db
-{
+class Db {
 
-    function __construct() {
-       // echo '<br>' . 'I am class Db.php';
+    protected $db;
+
+    public function __construct() {
+        $config = require 'app/config/databank.php';
+        $this->db = new PDO('mysql:host='.$config['host'].';dbname='.$config['dbname'].'', $config['user'], $config['password']);
     }
 
+    public function query($sql){
+        $query = $this->db->query($sql);
+        return $query;
+    }
+
+    public function row($sql){
+        $result = $this->query($sql);
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function column($sql){
+        $result = $this->query($sql);
+        return $result->fetchColumn();
+    }
+        
 }
