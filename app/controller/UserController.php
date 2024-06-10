ebb28dc70d0443223be21cb160c42646d807b041
Danilkberlin <danilkravets@gmail.com>
2024-06-10 09:34:16 +0200

    Rewrote all the code that was written in register.php in UserController.php

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -2,14 +2,45 @@
 
 namespace app\controller;
 use app\core\Controller;
+use app\lib\Db;
+use PDO;
 
 
 class UserController extends Controller {
 
+        
     public function indexAction(){
-        //$result = $this->model->insertEmail();
         $this->view->render('User');
     }
-    
+
+    public function registerAction(){
+        $this->view->render('Register');
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $login = $_POST['login'];
+            $password = $_POST['password'];
+            $repeatpassword = $_POST['repeatpassword'];
+            $email = $_POST['email'];
+        
+            if ($password !== $repeatpassword) {
+                die('Passwords do not match!');
+            }
+        
+            try {
+                $db = new Db();
+                $sql  = "INSERT INTO `users` (login, password, email) VALUES (:login, :password, :email)";
+                $result = $db->getConnection()->prepare($sql);
+                $result->bindParam(':login', $login, PDO::PARAM_STR);
+                $result->bindParam(':password', $password, PDO::PARAM_STR);
+                $result->bindParam(':email', $email, PDO::PARAM_STR);
+                $result->execute();
+                
+                echo 'Are you registered!';
+            } catch (PDOException $e) {
+                echo 'Database error: ' . $e->getMessage();
+            }
+        }
+    }
 
 }
+
