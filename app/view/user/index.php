bb5bce6aecfc60fea66cbb25f92212e2422f6139
Danilkberlin <danilkravets@gmail.com>
2024-06-10 09:36:29 +0200

    default html

--- app/view/user/index.php
+++ app/view/user/index.php
@@ -1,13 +1,34 @@
-<ol>
+<!-- <ol>
     <li>Zeige alle user an</li>
     <li>Kreiere einen User</li>
     <li>Lösche einen User</li>
-</ol>
+</ol> -->
 
-<form action="" method="post">
-    <div class="mb-3">
-        <label for='email'>Your Emal</label>
-        <input name='email' class="form-control w-50 mb-3" type='email' id="email">
-        <button type="submit" class='btn btn-primary py-2 px-5'>Submit</button>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-6 col-md-8 col-sm-10">
+            <h1 class='text-center'>Sign up</h1>
+            <form action="/user/register" method="post">        
+                <label for='login'>Your Login</label>
+                <input name='login' class="form-control w-100 mb-3" type='login' id="login">
+
+                <label for='password'>Your Password</label>
+                <input name='password' class="form-control w-100 mb-3" type='password' id="password">
+
+                <label for='repeatpass'>Your Repeat Password</label>
+                <input name='repeatpassword' class="form-control w-100 mb-3" type='password' id="repeatpass">
+
+                <label for='email'>Your Email</label>
+                <input name='email' class="form-control w-100 mb-3" type='email' id="email">
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        <button type="submit" class='btn btn-primary w-100 py-2 px-5'>Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-</form>
+</div>
+
+
