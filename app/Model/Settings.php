e51db71ea2b9450efef6c8e17fbe95cc90bc1cdc
Danilkberlin <danilkravets@gmail.com>
2024-06-12 09:32:26 +0200

    I wrote a function for the user so that he could change his data

--- app/Model/Settings.php
+++ app/Model/Settings.php
@@ -5,4 +5,8 @@ use app\core\Model;
 
 class Settings extends Model {
 
+    public function updateUser($user_id){
+        $result = $this->db->query("UPDATE users SET login=:login, password=:password, email=:email where id='$user_id'");
+        return $result;
+    }
 }
--- app/controller/SettingsController.php
+++ app/controller/SettingsController.php
@@ -6,7 +6,10 @@ use app\core\Controller;
 class SettingsController extends Controller {
 
     public function profileAction(){
-       
+        if(isset($_POST['user_id'])) {
+            $user_id = $_POST['user_id'];
+            $update_user = $this->model->updateUser($user_id);
+        }
         $this->view->render('Your profiel');
     }
 }
