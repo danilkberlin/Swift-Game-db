607e54f8e74773c17a12c6d9df9dc4ebda76cd3d
Danilkberlin <danilkravets@gmail.com>
2024-06-12 09:34:11 +0200

    Wrote a navigation bar

--- app/view/main/index.php
+++ app/view/main/index.php
@@ -1,6 +1,50 @@
 <header class="header">
+    <nav id="NavBar" class="navbar navbar-expand-lg navbar-light rounded" aria-label="Twelfth navbar example">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><!-- img --></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDrop" aria-controls="navbarDrop" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse justify-content-between" id="navbarDrop">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item m-2">
+                        <a class="nav-link rounded-2 px-4" href="#">Home</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link rounded-2 px-4" href="#">About</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link rounded-2 px-4" href="#">Tournaments</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link rounded-2 px-4" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link rounded-2 px-4" href="#">News</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link rounded-2 px-4" href="#">Contacts</a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <a href="user/user">
+                        <button class="btn btn-outline-success w-100 px-5">Login</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>
 <main class="main">
-    <p class='fs-1 text-center'>Home</p>
+    <!-- Top -->
+    <section id="top">
+
+    </section>
+    <!-- About -->
+    <section id='about'>
+
+    </section>
+
 </main>
 <footer class="footer"></footer>
