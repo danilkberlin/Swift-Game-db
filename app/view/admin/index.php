d2c2947045dab3a5ef3051b3654a838bceae6655
Danilkberlin <danilkravets@gmail.com>
2024-06-12 12:12:50 +0200

    Created a central slide for the administrator

---
+++ app/view/admin/index.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-9 col-md-10 col-sm-12 mb-5">
+            <h1 class='text-center'>Admin pages</h1>
+        </div>
+        <div class="col-4">
+            <a href="admin/delete">
+                <button class='btn btn-primary w-100'>Admit tabel</button>
+            </a>
+        </div>
+        <div class="col-4">
+            <a href="admin/login">
+                <button class='btn btn-primary w-100'>Analytical tabel</button>
+            </a>
+        </div>
+    </div>
+</div>
